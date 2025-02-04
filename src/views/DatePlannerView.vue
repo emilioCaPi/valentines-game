90c8464310f8a555e204c94eb635cc1a0b099b09
Emilio Carbonell <emilio.carbonell@hotmail.com>
2025-02-04 16:14:10 +0100

    feat: store other option value properly

--- src/views/DatePlannerView.vue
+++ src/views/DatePlannerView.vue
@@ -10,7 +10,6 @@ import { ref, watch } from 'vue'
 export interface IDateOption {
   key: string
   label: string
-  value: string
 }
 
 const doneButtonProtocol = ref<IButtonProtocol>({
@@ -21,39 +20,55 @@ const doneButtonProtocol = ref<IButtonProtocol>({
 })
 
 const dateOptions = ref<IDateOption[]>([
-  { key: 'Ir', label: 'Ir a comer 🍽', value: 'Ir a comer 🍽' },
-  { key: 'Br', label: 'Brunch 🥐', value: 'Brunch 🥐' },
-  { key: 'Ro', label: 'Road trip 🏎️', value: 'Road trip 🏎️' },
-  { key: 'De', label: 'Delis 🤪', value: 'Delis 🤪' },
-  { key: 'Ot', label: 'Otro', value: '' },
+  { key: 'Ir', label: 'Ir a comer 🍽' },
+  { key: 'Br', label: 'Brunch 🥐' },
+  { key: 'Ro', label: 'Road trip 🏎️' },
+  { key: 'De', label: 'Delis 🤪' },
+  { key: 'Ot', label: 'Otro' },
 ])
 
 const defaultOption = ref<IDateOption>(
   dateOptions.value.find((option) => option.key === 'De') ?? {
     key: 'De',
     label: 'Delis 🤪',
-    value: 'Delis 🤪',
   },
 )
 
-const otherOptionSelected = ref<boolean>(false)
-const otherOption = ref<IDateOption>({ key: 'Ot', label: '', value: '' })
+const isOtherOptionSelected = ref<boolean>(false)
+const otherOption = ref<string>('')
 
 const selectedOption = ref<IDateOption[]>([defaultOption.value])
 
-watch(selectedOption, (newVal) => {
-  if (newVal.length > 2) {
-    selectedOption.value = [defaultOption.value, newVal[newVal.length - 1]]
+watch([selectedOption, otherOption], ([newSelectedVal, newOtherOptionVal]) => {
+  const hasTwoOptions = newSelectedVal.length >= 2
+  isOtherOptionSelected.value = newSelectedVal.some((opt) => opt.key === 'Ot')
+  const isOtherOptionValid = newOtherOptionVal.trim().length >= 3
+
+  /**
+   * Up to two options can be chosen
+   */
+  if (newSelectedVal.length > 2) {
+    selectedOption.value = [defaultOption.value, newSelectedVal[newSelectedVal.length - 1]]
   }
 
-  otherOptionSelected.value = Boolean(newVal.find((option) => (option.key === 'Ot' ? true : false)))
+  /**
+   * Clear `otherOption` if is not selected
+   */
+  isOtherOptionSelected.value = newSelectedVal.some((opt) => opt.key === 'Ot')
+  if (!isOtherOptionSelected.value) {
+    otherOption.value = ''
+  }
 
   /**
    * Enabling / Disabling submit button
-   * Default length 1 = 1 < 2 = true = its disabled then
-   * Second option check = length 2 = 2 < 2 = false = its not disabled then
+   *
+   * At least two option need to be selected, without the 'other' option selected OR
+   * Is the 'other' option selected and it has at least 3 characters
    */
-  doneButtonProtocol.value.disabled = newVal.length < 2
+  const isOtherValid = isOtherOptionSelected.value && isOtherOptionValid
+  const hasEnoughOptions = hasTwoOptions && !isOtherOptionSelected.value
+
+  doneButtonProtocol.value.disabled = !(isOtherValid || hasEnoughOptions)
 })
 
 const messageStore = useMessageStore()
@@ -61,7 +76,9 @@ const summaryStore = useSummaryStore()
 
 const submit = () => {
   disableBtn()
-  summaryStore.setDateResult(selectedOption.value)
+
+  const result = getResultOptions()
+  summaryStore.setDateResult(result)
 
   setTimeout(() => {
     doneButtonProtocol.value.loading = false
@@ -69,6 +86,14 @@ const submit = () => {
   }, 3000)
 
   updateMessage()
+}
+
+function getResultOptions(): IDateOption[] {
+  return selectedOption.value.map((option) =>
+    option.key === 'Ot' && otherOption.value.trim()
+      ? { key: 'Cu', label: otherOption.value }
+      : option,
+  )
 }
 
 const disableBtn = () => {
@@ -100,9 +125,9 @@ const updateMessage = () => {
               {{ option.label }}
             </label>
           </div>
-          <div v-if="otherOptionSelected">
+          <div v-if="isOtherOptionSelected">
             <input
-              v-model="otherOption.label"
+              v-model="otherOption"
               type="text"
               placeholder="Enter other option"
               class="mb-[0.5rem] w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
