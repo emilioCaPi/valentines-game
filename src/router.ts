53b26c42c9f1601ef8eb1f8b5b206e6823fc3470
Emilio Carbonell <emilio.carbonell@hotmail.com>
2025-02-04 11:36:41 +0100

    fix: added all routes

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,10 @@ import DatePlannerView from './views/DatePlannerView.vue'
 import SummaryView from './views/SummaryView.vue'
 
 const routes = [
-  // { path: '/', component: IntroGameView },
-  // { path: '/game', component: GameView },
-  // { path: '/date', component: DatePlannerView },
-  { path: '/', component: DatePlannerView },
-  // { path: '/summary', component: SummaryView },
+  { path: '/', component: IntroGameView },
+  { path: '/game', component: GameView },
+  { path: '/date', component: DatePlannerView },
+  { path: '/summary', component: SummaryView },
 ]
 
 const router = createRouter({
